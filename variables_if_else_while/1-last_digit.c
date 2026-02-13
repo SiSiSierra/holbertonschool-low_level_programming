03cfc59796e5ae180df37153f2a0d81119cf238b
Sierra <SierraHunt@protonmail.com>
2026-02-13 07:07:07 +0000

    Add documentation

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -3,7 +3,11 @@
 #include <stdio.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
 	int n;
