074f3852d5efcac3c57fae9057c7d76bb7052ee5
Sierra <SierraHunt@protonmail.com>
2026-02-16 23:53:32 +0000

    Fixed typo

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _isint - Check if c is an ASCII digit
+ * _isdigit - Check if c is an ASCII digit
  *
  * @c: The ASCII code to check
  * Return: 1 if a digit, 0 otherwise
  */
-int _isint(int c)
+int _isdigit(int c)
 {
 	if (c >= 48 && c <= 57)
 	{
