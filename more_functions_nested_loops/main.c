1b9f2a27fd2cd30012ba6798da354faaa97bf293
Sierra <SierraHunt@protonmail.com>
2026-02-16 23:39:51 +0000

    2nd file for 2nd task

--- more_functions_nested_loops/main.c
+++ more_functions_nested_loops/main.c
@@ -3,10 +3,16 @@
 
 int main(void)
 {
+	printf("Task 0 -----\n");
 	printf("%d", _isupper(64));
 	printf("%d", _isupper(65));
 	printf("%d", _isupper(90));
 	printf("%d", _isupper(91));
+	printf("\n");
+
+	printf("Task 1 -----\n");
+	printf("%d%d%d%d", _isdigit(47), _isdigit(48), _isdigit(57), _isdigit(58));
+	printf("\n");
 
 	return (0);
 }
