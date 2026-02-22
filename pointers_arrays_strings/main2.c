87071062639fdf571ece3564f2675fc8ca73e37d
Sierra <SierraHunt@protonmail.com>
2026-02-22 09:53:53 +0000

    File for task 3 of part 2

--- pointers_arrays_strings/main2.c
+++ pointers_arrays_strings/main2.c
@@ -10,12 +10,16 @@ int main(void)
 {
 	char s1[98] = "Hello ";
 	char s2[] = "World!";
+	char s3[] = "Hello";
+	char s4[] = "Helloo";
 
 	printf("%s\n", _strcat(s1, s2));
 
 	printf("%s\n", _strncat(s1, s2, 2));
 
 	printf("%s\n", _strncpy(s1, s2, 5));
+
+	printf("%d\n", _strcmp(s3, s4));
 
 	return (0);
 }
