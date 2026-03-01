022ee13ff16acf6b5fbcd50d89fad0e4ce25f3b0
Sierra <SierraHunt@protonmail.com>
2026-03-01 12:07:39 +0000

    File for task 4

---
+++ argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry
+ *
+ * @argc: Number of commands
+ * @argv: commands
+ * Return: 0, 1 if error
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i;
+
+	for (i = 1; i < argc; i++)
+	{
+		int j;
+
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < 48 || argv[i][j] > 57)
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
