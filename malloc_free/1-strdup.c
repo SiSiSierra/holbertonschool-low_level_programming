3916ee9554da582c49be9c7effc5b09010a5c055
Sierra <SierraHunt@protonmail.com>
2026-03-02 07:55:48 +0000

    File for task 1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - Copy a string into new memory
+ *
+ * @str: Given string
+ * Return: Pointer to new string
+ */
+char *_strdup(char *str)
+{
+	int i;
+	char *array;
+	int size;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	}
+
+	size = i;
+	array = malloc(size);
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; str[i] < size; i++)
+	{
+		array[i] = str[i];
+	}
+	return (array);
+}
--- malloc_free/main.c
+++ malloc_free/main.c
@@ -10,13 +10,21 @@
 int main(void)
 {
 	char *array;
+	char *array2;
 
 	array = create_array(10, 66);
 	if (array == NULL)
 	{
-		printf("Array creation failed");
+		printf("Array creation failed\n");
 	}
 
+	printf("%c%c%c\n", array[0], array[4], array[9]);
+
+	array2 = _strdup(array);
+	if (array2 == NULL)
+	{
+		printf("Array creation failed\n");
+	}
 	printf("%c%c%c\n", array[0], array[4], array[9]);
 
 	return (0);
