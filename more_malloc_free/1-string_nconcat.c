b69414b0684fe764070498778308a54d4d5407e9
Sierra <SierraHunt@protonmail.com>
2026-03-06 08:45:38 +0000

    Allocated memory for terminating character

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -25,7 +25,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; s2[i] != '\0' && i < n; i++)
 	{}
 	n = i;
-	str = malloc(s1len + n);
+	str = malloc(s1len + n + 1);
 	if (str == NULL)
 		return (NULL);
 	for (i = 0; i < s1len; i++)
