54caa63d124671cd6cfe097fa4000370b83cf359
Sierra <SierraHunt@protonmail.com>
2026-03-08 11:17:08 +0000

    Freed memory and copied strings

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * new_dog - Create a dog struct using dog_t type
@@ -12,14 +13,34 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
+	char *n;
+	char *o;
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
+	n = malloc(strlen(name) + 1);
+	if (n == NULL)
+		return (NULL);
+
+	strcpy(n, name);
+
+	o = malloc(strlen(owner) + 1);
+	if (o == NULL)
+	{
+		free(n);
+		return (NULL);
+	}
+	strcpy(o, owner);
 
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
+	{
+		free(o);
+		free(n);
 		return (NULL);
-	if (name == NULL || owner == NULL)
-		return (NULL);
-	d->name = name;
+	}
+	d->name = n;
 	d->age = age;
-	d->owner = owner;
+	d->owner = o;
 	return (d);
 }
