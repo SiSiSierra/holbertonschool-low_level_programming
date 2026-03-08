f8123850db2ddaa31e39a43c3b613e6f33bdd59c
Sierra <SierraHunt@protonmail.com>
2026-03-08 09:41:04 +0000

    Initialise directory and file for task 0

---
+++ structures_typedef/README.md
@@ -0,0 +1,2 @@
+#Structures
+In this directory I am learning to make data structures to organise data!
---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - Doggy data
+ * @name: Doggy name
+ * @age: Doggy age
+ * @owner: Doggy owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
