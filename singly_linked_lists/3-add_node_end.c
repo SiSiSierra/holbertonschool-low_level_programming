14c4a5b11f958556e7b4aaf50dd66118e2a569f4
Sierra <SierraHunt@protonmail.com>
2026-03-28 10:08:32 +0000

    File for task 3

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,50 @@
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * add_node_end - Add a node at the end of a linked list
+ *
+ * @head: Pointer to head of list
+ * @str: String to put in node
+ * Return: Address of new node
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new;
+	list_t *end;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	new->len = 0;
+	while (new->str[new->len] != '\0')
+		new->len++;
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		end = *head;
+		while (end->next != NULL)
+			end = end->next;
+		end->next = new;
+	}
+	return (new);
+
+}
+
+/**
+ * find_end - Get pointer to last node in linked list
+ *
+ * @head: Pointer to head of list
+ * Return: Pointer to end of list
+ */
+list_t *find_end(list_t *head)
+{
+	if (head == NULL)
+		return (NULL);
+	if (head->next == NULL)
+		return (head);
+	return (find_end(head));
+}
