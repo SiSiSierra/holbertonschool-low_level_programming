bd44cafa85a1e8d2e9a45e706449dda94cb33717
Sierra <SierraHunt@protonmail.com>
2026-03-28 12:13:13 +0000

    File for task 6

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * sum_dlistint - Sum the contents of each node in linked list
+ *
+ * @head: Head of linked list
+ * Return: Sum of member n
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *node = head;
+	int n = 0;
+
+	if (node == NULL)
+		return (0);
+	n += node->n;
+	while (node->next != NULL)
+	{
+		node = node->next;
+		n += node->n;
+	}
+	return(n);
+}
